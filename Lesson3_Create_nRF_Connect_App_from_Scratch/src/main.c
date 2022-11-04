89c3dcf0163fd802c9c2384e39791493c3687a3c
Zeki <nguyenhatrung411@gmail.com>
2022-11-04 21:38:50 +0900

    Update Lesson 3 README

--- Lesson3_Create_nRF_Connect_App_from_Scratch/src/main.c
+++ Lesson3_Create_nRF_Connect_App_from_Scratch/src/main.c
@@ -1,10 +1,35 @@
 #include <zephyr.h>
 #include <sys/printk.h>
+#include "myfunction.h"
+
+// void main(void)
+// {
+// 	while(1) {
+// 		printk("Hello World!\n\r");
+// 		k_msleep(1000);
+// 	}
+// }
+
+// void main(void)
+// {
+// 	int a = 3, b = 4;
+// 	while(1){
+// 		printk("The sum of %d and %d is %d\n\r", a, b, sum(a,b));
+// 		k_msleep(1000);
+// 	}
+// }
 
 void main(void)
 {
-	while(1) {
-		printk("Hello World!\n\r");
+	int a = 3, b = 4;
+	while(1){
+		if(!(IS_ENABLED(CONFIG_MYFUNCTION))){
+			printk("MYFUNCTION not enabled\r\n");
+			return;
+		}
+		printk("The sum of %d and %d is %d\n\r", a, b, sum(a,b));
 		k_msleep(1000);
 	}
 }
+
+
